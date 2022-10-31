484374e6b80a64f978e481e3fc1ec999668d0391
Irinaabieva <irinaabieva03@gmail.com>
2022-10-31 13:22:58 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
         var dog = 8;
         var cat = 3.6;
         var paper = 763789;
-        dog += 4;
-        cat += 4;
-        paper +=4;
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
